429f05bf24c946c5c0cde9aca70b255af7b366cd
MaroM <maro.muszynski@gmail.com>
2016-06-21 09:28:44 +0200

    create home and forecast controllers

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 // Modules
 var weatherApp = angular.module("weatherApp", ['ngRoute', 'ngResource']);
 
+// Controller
+weatherApp.controller('homeController', ['$scope', function($scope) {
+    
+}]);
+
+weatherApp.controller('forecastController', ['$scope', function($scope) {
+    
+}]);
