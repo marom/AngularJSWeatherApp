e3471883fd2c03ce3fb95053175fa0f7d81a7bd4
MaroM <maro.muszynski@gmail.com>
2016-06-21 15:19:53 +0200

    improve UX by allowing a user to submit a search by pressing a Enter key on a page

--- app.js
+++ app.js
@@ -27,13 +27,17 @@ weatherApp.service('cityService', function() {
 });
 
 // Controller
-weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+weatherApp.controller('homeController', ['$scope', '$location', 'cityService', function($scope, $location, cityService) {
     
     $scope.city = cityService.city;
     
     $scope.$watch('city', function() {
         cityService.city = $scope.city;
     });
+    
+    $scope.submit = function() {
+        $location.path("/forecast")
+    };
     
 }]);
 
